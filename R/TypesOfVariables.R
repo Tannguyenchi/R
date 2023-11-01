c80c7b48f2fb97f68a8bee54b4654e53b13b2a2b
Tannguyenchi <44901865+Tannguyenchi@users.noreply.github.com>
2023-11-01 13:31:20 +0700

    Initial commit

---
+++ R/TypesOfVariables.R
@@ -0,0 +1,21 @@
+ #int
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3 +2i
+typeof(z)
+
+#character
+a <- "hello"
+typeof(a)
+
+#logical
+l1 <- T
+typeof(l1)
+l2 <- F
+typeof(l2)
---
+++ R/UsingVariables.R
@@ -0,0 +1,19 @@
+A <- 10
+B <- 5
+C <- A + B #Sum
+C
+var1 <- 2.5
+var2 <- 4
+typeof(var1)
+typeof(var2)
+
+result <- var1/var2
+result
+
+answer <- sqrt(var2)
+answer
+
+greeting <- "Hello"
+name <- "Bod"
+message <- paste(greeting,name)
+message
